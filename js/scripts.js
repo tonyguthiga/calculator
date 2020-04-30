5dbba0d123da90cc1b705618f7324c18799d4c7a
antony-guthiga <toniguthiga93@gmail.com>
2020-04-30 14:27:54 +0300

    added a new function and deleted the previous

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,5 @@ var add = function(number1, number2) {
     };
     var number1 = parseInt(prompt("Enter a number: "));
     var number2 = parseInt(prompt("Enter another number: "));
-    alert(add(number1, number2));
+    var result = add(number1, number2);
+    alert(result);
