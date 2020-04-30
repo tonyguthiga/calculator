31816c583ac1fd2311c223771525259586dd0ca9
antony-guthiga <toniguthiga93@gmail.com>
2020-04-30 14:45:26 +0300

    added multiplication function

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,11 @@ var subtract = function(number1, number2) {
     var number2 = parseInt(prompt("Enter another number: "));
     var result = subtract(number1, number2);
     alert(result);
+
+var multiply = function(number1, number2) {
+    return number1 * number2;
+    };
+    var number1 = parseInt(prompt("Enter a number: "));
+    var number2 = parseInt(prompt("Enter another number: "));
+    var result = multiply(number1, number2);
+    alert(result);
