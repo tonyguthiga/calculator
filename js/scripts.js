c907c11db30d1248aa20dcf9b51daf8bf331fb2e
antony-guthiga <toniguthiga93@gmail.com>
2020-04-30 14:50:06 +0300

    added division function

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,11 @@ var multiply = function(number1, number2) {
     var number2 = parseInt(prompt("Enter another number: "));
     var result = multiply(number1, number2);
     alert(result);
+
+var divide = function(number1, number2) {
+    return number1 / number2;
+    };
+    var number1 = parseInt(prompt("Enter a number: "));
+    var number2 = parseInt(prompt("Enter another number: "));
+    var result = divide(number1, number2);
+    alert(result);
